1bea20a1564d84267a06e7616c8c98986ab49f6c
zhouhuan <towonzhou@gmail.com>
2016-02-25 10:51:54 +0800

    remove test code

--- lib/materialui-rails/railtie.rb
+++ lib/materialui-rails/railtie.rb
@@ -1,7 +1,6 @@
 # -*- encoding: UTF-8 -*-
 module MaterialuiRails
   class Railtie < Rails::Railtie
-    railtie_name :materialUi_rails
     rake_tasks do
       Dir[File.join(File.dirname(__FILE__), "tasks/*.rake")].each { |f| load f }
     end
